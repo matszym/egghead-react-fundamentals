bcb5c92d635ad85bda5030f797af2ed583f5da5a
Mateusz Szymański <hello`at`mateuszszymanski`dot`eu (remove ` from address, at === @, dot === .)>
2016-10-07 13:12:48 +0200

    Owner ownie relationship

--- App.js
+++ App.js
@@ -2,22 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { txt: '' };
+  }
+  update(e) {
+    this.setState({txt: e.target.value});
+  }
   render() {
     let txt = this.props.txt;
 
     return (
-      <h1>{this.props.txt}</h1>
+      <Widget txt={this.state.txt} update={ e => this.update(e) } />
     )
   }
 }
 
-App.propTypes = {
-  txt: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-  txt: 'this is the default txt'
+const Widget = props => {
+  return (
+    <div>
+      <input type="text"
+        onChange={ props.update } />
+      <h1>{ props.txt }</h1>
+    </div>    
+  )
 }
 
 ReactDOM.render(
