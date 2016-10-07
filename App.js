7fe594ea6bcf2f732429c06640e668db669b0902
Mateusz Szymański <hello`at`mateuszszymanski`dot`eu (remove ` from address, at === @, dot === .)>
2016-10-07 09:34:05 +0200

    react props default and requiring

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   render() {
+    let txt = this.props.txt;
+
     return (
-      <div>Hey!</div>
+      <h1>{this.props.txt}</h1>
     )
   }
 }
 
-export default App;
+App.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+  txt: 'this is the default txt'
+}
+
+ReactDOM.render(
+  <App cat={5} />,
+  document.getElementById('app')
+);
