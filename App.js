526dea5900f86a8a39509850d7a1e7ca238d7e49
Mateusz Szymański <hello`at`mateuszszymanski`dot`eu (remove ` from address, at === @, dot === .)>
2016-10-07 19:32:32 +0200

    transclude equivalent in react

--- App.js
+++ App.js
@@ -2,48 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = { txt: '' };
-  }
-  update(e) {
-    this.setState({
-      red: ReactDOM.findDOMNode(this.refs.red).value,
-      green: ReactDOM.findDOMNode(this.refs.green).value,
-      blue: ReactDOM.findDOMNode(this.refs.blue).value
-    });
-  }
   render() {
-    let txt = this.props.txt;
-
     return (
-      <div>
-        <Slider ref="red" update={e => this.update(e)} />
-        {this.state.red}
-        <br />
-        <Slider ref="green" update={e => this.update(e)} />
-        {this.state.green}
-        <br />
-        <Slider ref="blue" update={e => this.update(e)} />
-        {this.state.blue}
-        <br />
-      </div>
+      <Button>I <Heart /> React</Button>
     )
   }
 }
 
-class Slider extends React.Component {
+class Button extends React.Component {
+  componentDidMount() {
+    console.log(this.props.children);
+  }
   render() {
-    return (
-        <input 
-          type="range"
-          min="0"
-          max="255"
-          onChange={ this.props.update } 
-        />
-    )
+    // this.props.children 
+    // is basically poor man's transclude equivalent
+    return <button>{this.props.children}</button>
   }
 }
+
+const Heart = () => <span className="glyphicon glyphicon-heart"></span>
 
 ReactDOM.render(
   <App cat={5} />,
